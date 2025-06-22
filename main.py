6eed452b6b4d339840a8e6019f25e1b8985401f1
Rendell Arruda <rendellarruda3@gmail.com>
2025-06-22 11:14:57 -0300

    feat: bucktes sem lifecycle separados por virgula

--- main.py
+++ main.py
@@ -4,12 +4,13 @@ profiles= ["sandbox", "default"]
 
 def get_buckets_without_lifecycle(session):
     s3 = session.client('s3')
-    buckets_without_lifecycle = []
+    # buckets_without_lifecycle = []
 
     try:
         # Lista todos os buckets
         response = s3.list_buckets()
         buckets = response.get('Buckets', [])
+        print(f"Total de buckets encontrados: {len(buckets)}")
         
         for bucket in buckets:
             bucket_name = bucket['Name']
@@ -19,23 +20,25 @@ def get_buckets_without_lifecycle(session):
             except s3.exceptions.ClientError as e:
                 # Adiciona o bucket à lista se não houver política de lifecycle
                 if e.response['Error']['Code'] == 'NoSuchLifecycleConfiguration':
-                    buckets_without_lifecycle.append(bucket_name)
+                    print(bucket_name, end=',')
+                    # buckets_without_lifecycle.append(bucket_name)
     except Exception as e:
         print(f"Erro ao listar buckets: {e}")
 
-    return buckets_without_lifecycle
+    # return buckets_without_lifecycle
 
 for profile in profiles:
+    print("\n")
     print(f"Verificando perfil: {profile}")
     session = boto3.Session(profile_name=profile)
     buckets = get_buckets_without_lifecycle(session)
     
-    if buckets:
-        print("Buckets sem política de lifecycle:")
-        for bucket in buckets:
-            print(f"- {bucket}")
-    else:
-        print("Todos os buckets têm política de lifecycle configurada.")
+    # if buckets:
+    #     print("Buckets sem política de lifecycle:")
+    #     for bucket in buckets:
+    #         print(f"- {bucket}")
+    # else:
+    #     print("Todos os buckets têm política de lifecycle configurada.")
     
     
 # if __name__ == '__main__':
